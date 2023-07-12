8a35219844ca8abb951f7ca8867b700f44350f93
Jack Lehner <jacklehner44@yahoo.com>
2023-07-12 14:47:32 -0400

    github finder app

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,25 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext()
+
+export const GithubProvider = ({children}) => {
+    const initialState ={
+        users: [],
+        loading: false,
+        user: [],
+        repos: [],
+
+    }
+
+    const [state, dispatch] = useReducer(githubReducer, initialState) //dispatch action to reducer
+
+    return <GithubContext.Provider value={{
+        ...state,
+        dispatch,
+    }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export default GithubContext
